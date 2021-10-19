c4a6eb47f4d73a516e61d57162ad9395abbadc18
michaelhebner <mhebner@student.bridgew.edu>
2021-10-19 16:27:46 -0400

    Brainstormed some classes

--- model.py
+++ model.py
@@ -1,2 +1,28 @@
+class Person(object):
 
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def is_instructor(self):
+        return False
+
+
+class Instructor(Person):
+
+    def is_instructor(self):
+        return True
+
+
+class Student(Person):
+
+    def is_instructor(self):
+        return False
+
+
+class Registration:
+    pass
 
