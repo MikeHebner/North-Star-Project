c372764f0863e0a804386eefa2e4fecb6bd4c72c
michaelhebner <mhebner@student.bridgew.edu>
2021-10-26 20:10:03 -0400

    Brainstormed some classes

--- model.py
+++ model.py
@@ -1,28 +1,42 @@
-class Person(object):
+class Student(object):
 
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+    def __init__(self, student_id, student_name):
+        self.student_id = student_id
+        self.student_name = student_name
 
     def get_name(self):
-        return self.name
-
-    def is_instructor(self):
-        return False
+        return self.student_name
 
 
-class Instructor(Person):
+class Instructor(object):
 
-    def is_instructor(self):
-        return True
+    def __init__(self, instructor_id, instructor_name):
+        self.instructor_id = instructor_id
+        self.instructor_name = instructor_name
 
-
-class Student(Person):
-
-    def is_instructor(self):
-        return False
+    def get_name(self):
+        return self.instructor_name
 
 
-class Registration:
-    pass
+class Enrollment(object):
+
+    def __init__(self, student_id, section_id, flag):
+        self.student_id = student_id
+        self.section_id = section_id
+        self.flag = None
+
+    def addFlag(self):
+        pass
+
+    def removeFlag(self):
+        pass
+
+
+class Section(object):
+    def __init__(self, section_id, capacity, credits):
+        self.section_id = section_id
+        self.capacity = capacity
+        self.credits = credits
+
+
 
