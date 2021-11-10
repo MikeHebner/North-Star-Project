e0654348986297042b59aca0f2c34b6f4e8142e0
rkuczer <57370393+rkuczer@users.noreply.github.com>
2021-11-10 15:24:05 -0500

    test

--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-
-
 class Student:
 
     def __init__(self, student_id, student_name):
@@ -9,30 +7,23 @@ class Student:
     def __str__(self):
         return self.student_id + " : " + self.student_name
 
-
 class Instructor:
-
     def __init__(self, instructor_id, instructor_name):
         self.instructor_id = instructor_id
         self.instructor_name = instructor_name
-
     def __str__(self):
         return self.instructor_id + " : " + self.instructor_name
 
 
 class Enrollment:
-
     def __init__(self, student_id, section_id):
         self.student_id = student_id
         self.section_id = section_id
         self.flag = []
-
     def addFlag(self, flag):
         self.flag.append(flag)
-
     def removeFlag(self, flag):
         self.flag.remove(flag)
-
 
 class Section:
     def __init__(self, section_id, capacity, credits):
