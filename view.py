cf873f3f008c3a6f3a514f115376d5927719a3e9
rkuczer <ryan-kooz@hotmail.com>
2021-11-16 15:36:21 -0500

    imports and testing qt designer

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore, QTGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QWidget, QPushButton
 from PyQt5.QtCore import pyqtSlot
 import PyQt5.QtWidgets as qtw
