5a7a7d35acfbb0f609a4ce83db09ae9d604a5ba2
rkuczer <57370393+rkuczer@users.noreply.github.com>
2021-11-17 14:52:01 -0500

    created methods for initializing ui

--- view.py
+++ view.py
@@ -12,36 +12,51 @@ class AddStudent(QWidget):
         self.setWindowTitle('Add Student')
         self.resize(200, 200)
         layout = QVBoxLayout()
-        self.textbox = QLineEdit(self)
-        self.textbox.setPlaceholderText("Enter Student Name")
-        self.textbox.move(20, 20)
-        self.textbox2 = QLineEdit(self)
-        self.textbox2.setPlaceholderText("Enter Student ID")
-        self.textbox2.move(20, 60)
-        self.b1 = QPushButton("Add Student", self)
-        layout.addWidget(self.b1)
-        self.b1.resize(30,100)#error button not resizing cant figure out
-        self.b1.move(200, 50)
+        self.initUI()
         self.setLayout(layout)
-        self.show()
+
+    def initUI(self):
+        textbox = QLineEdit(self)
+        textbox.setPlaceholderText("Enter Student Name")
+        textbox.move(20, 20)
+        textbox2 = QLineEdit(self)
+        textbox2.setPlaceholderText("Enter Student ID")
+        textbox2.move(20, 60)
+        button = QPushButton("Add Student", self)
+        # error button not resizing cant figure out
+        button.move(200, 50)
+
 
 
 class AddInstructor(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Add Instructor')
+        self.resize(200, 200)
         layout = QVBoxLayout()
-        self.label = QLabel("Another Window2")
-        layout.addWidget(self.label)
+        self.initUi()
         self.setLayout(layout)
-
+    def initUi(self):
+        textbox = QLineEdit(self)
+        textbox.setPlaceholderText("Enter Instructor Name")
+        textbox.move(20, 20)
+        textbox2 = QLineEdit(self)
+        textbox2.setPlaceholderText("Enter Instructor ID")
+        textbox2.move(20, 60)
 
 class editDatabase(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Choose Database")
+        self.resize(200,200)
         layout = QVBoxLayout()
-        self.label = QLabel("Another Window3")
-        layout.addWidget(self.label)
         self.setLayout(layout)
+        self.initUI()
+    def initUI(self):
+        button = QPushButton("Student Database", self)
+        button.move(20,50)
+        button2 = QPushButton("Faculty Database", self)
+        button2.move(20, 100)
 
 
 class MainWindow(QMainWindow):
@@ -64,7 +79,7 @@ class MainWindow(QMainWindow):
         pybutton2.resize(100, 32)
         pybutton2.move(50, 60)
 
-        pybutton3 = QPushButton('Edit Course', self)
+        pybutton3 = QPushButton('Edit Database', self)
         pybutton3.clicked.connect(self.showEditDatabase)
         pybutton3.resize(100, 32)
         pybutton3.move(50, 100)
