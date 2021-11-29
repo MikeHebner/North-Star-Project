2d3786ccd958c0c71f52b2fc6e42b02765d135cf
spritesama29 <79071078+spritesama29@users.noreply.github.com>
2021-11-29 18:51:21 -0500

    Section now had add, remove, and editSectionInfo. Add takes in sectionID,capacity,CourseId, and instructorID and adds them to the section table. Before this, it checks if there is a valid courseID, then InstructorID, then checks for duplicate sections. Remove checks for a section with that sectionID, CourseID, and instructorID, and if true, deletes it from the table. EditStudentInfo Checks for a section with the specified SectionID and CourseID and if true, checks for valid InstructorID given, if true, then updates the instructor of the section with the given InstructorID.

--- model.py
+++ model.py
@@ -67,8 +67,50 @@ class Student:
             return 0
             # No ID of that in student
 
-    def getEnrolledCreds(self,studentID):
-        q = "SELECT "
+#
+# def add(status, ID, name):
+#     if status == "s":
+#         try:
+#
+#             q = "INSERT INTO Student(student_ID,student_name) VALUES (?,?)"
+#             conn.execute(q, (ID, name))
+#             conn.commit()
+#         except:
+#             print("That name or ID already exists in this table")
+#
+#
+#
+#     elif status == "i":
+#         try:
+#             q = "INSERT INTO Instructor(instructor_ID, instructor_name) VALUES (?,?)"
+#             conn.execute(q, (ID, name))
+#             conn.commit()
+#         except:
+#             print("That name or ID already exists in this table")
+#
+#
+# def remove(status, name):
+#     if status == "student":
+#
+#         q = "DELETE FROM Student WHERE student_name = (?)"
+#         conn.execute(q, (name,))
+#         conn.commit()
+#     elif status == "instructor":
+#         q = "DELETE FROM Instructor WHERE instructor_name = (?)"
+#         conn.execute(q, (name,))
+#         conn.commit()
+
+#
+# add("s", 123, "Bobby")
+# remove("student", "Bobby")
+# Student.add(123, "Bobby")
+# print(Student.remove("9045"))
+# add("student", 123, "Bobby1")
+# Student.editStudentInfo("OtherBobby", "123")
+# x = Student.getAll()
+# xSize = len(x)
+# for i in range(xSize):
+#     print(x[i])
 
 
 class Instructor:
@@ -123,7 +165,11 @@ class Instructor:
         else:
             return 0
 
-
+Instructor.editInstructorInfo("Kelly Manso",60)
+x = Instructor.getAll()
+xSize = len(x)
+for i in range(xSize):
+     print(x[i])
 
 class Enrollment:
     def __init__(self, student_id, section_id):
@@ -131,38 +177,74 @@ class Enrollment:
         self.section_id = section_id
         self.flag = None
 
-    @classmethod
-    def getCourseLink(cls, section_id, course_id):
-        q = "SELECT course_link FROM Section WHERE section_ID=? AND course_ID=?"
-        cursor = conn.execute(q,(section_id,course_id))
-        link = cursor.fetchone()
-        return link[0]
-
-    @classmethod
-    def add(cls,flag, student_id, course_link):
+    def add(cls,student_id, section_id, flag):
         q = "INSERT INTO Enrolls_in(flag, student_ID, course_link) VALUES (?,?,?)"
-        conn.execute(q,(flag,student_id,course_link))
-        conn.commit()
 
-    @classmethod
-    def checkCap(cls,course_link):
-        q = "SELECT capacity FROM Section WHERE course_link=?"
-        capacity = conn.execute(q, course_link)
-        return capacity.fetchall()[0]
 
-    @classmethod
-    def checkDuplicate(cls,student_id,course_link):
-        q = "SELECT COUNT(*) FROM Enrolls_in WHERE student_ID=? AND course_link=?"
-        response = conn.execute(q,(student_id,course_link))
-        return response.fetchone()[0]
 
-x = Enrollment.checkDuplicate(1,1)
-print(x)
 class Section:
-    def __init__(self, course_link, section_id, capacity,course_id, instructor_id ):
-        self.course_link = course_link
+    def __init__(self, section_id, capacity, course_id,instuctor_id):
         self.section_id = section_id
         self.capacity = capacity
         self.course_id = course_id
-        self.instructor_id = instructor_id
+        self.instructor_id = instuctor_id
 
+    @classmethod
+    def add(cls,section_id,capacity,course_id,instructor_ID):
+        q1 = "SELECT * FROM main.Course WHERE course_ID=(?)"
+        cursor = conn.execute(q1, (course_id,))
+        if len(cursor.fetchall()) > 0:
+            q2 = "SELECT * FROM main.Instructor WHERE instructor_ID=(?)"
+            cursor2 = conn.execute(q2, (instructor_ID,))
+            if len(cursor2.fetchall()) > 0:
+                q3 = "SELECT * FROM main.Section WHERE section_ID=(?) and course_ID=(?)"
+                cursor3 = conn.execute(q3, (section_id, course_id,))
+                if len(cursor3.fetchall()) > 0:
+                    return 2
+                    #Duplicate Section is trying to be entered
+                else:
+                    q4 = "INSERT INTO Section(section_ID,capacity,course_ID,instructor_ID) VALUES (?,?,?,?)"
+                    conn.execute(q4, (section_id,capacity,course_id,instructor_ID))
+                    conn.commit()
+                    return 1
+                    #Section is valid and added
+            else:
+                return 3
+                #No instructor has that ID
+        else:
+            return 4
+            #No Course has that ID
+    @classmethod
+    def remove(cls,section_id,course_id,instructor_ID):
+        q = "SELECT * FROM main.Section WHERE section_ID=(?) and course_ID=(?) and instructor_ID=(?)"
+        response = conn.execute(q, (section_id, course_id, instructor_ID))
+        if len(response.fetchall())>0:
+            q2 = "DELETE FROM main.Section WHERE section_ID=(?) and course_ID=(?) and instructor_ID=(?)"
+            conn.execute(q2, (section_id, course_id, instructor_ID))
+            conn.commit()
+            return 1
+            #Successfully found and deleted section
+        else:
+            return 0
+            #Did invalid section
+    @classmethod
+    def editSectionInfo(cls,section_ID,course_ID, instructor_ID):
+        q = "SELECT * FROM main.Section WHERE section_ID=(?) and course_ID=(?)"
+        response = conn.execute(q, (section_ID,course_ID,))
+        if len(response.fetchall())>0:
+            q2 = "SELECT * FROM main.Instructor WHERE instructor_ID=(?)"
+            response2 = conn.execute(q2, (instructor_ID,))
+            if len(response2.fetchall())>0:
+                q3 = "UPDATE Section Set instructor_ID=(?) WHERE main.Section.section_ID==(?) and main.Section.course_ID==(?)"
+                conn.execute(q3, (instructor_ID,section_ID,course_ID,))
+                conn.commit()
+                return 1
+            else:
+                return 2
+                #No Instructor with that ID
+        else:
+            return 3
+            #Section doesn't exist
+print(Section.add(13,3,"ART101",30))
+print(Section.remove(13,"ART101",10))
+print(Section.editSectionInfo(13,"ART101",10))
